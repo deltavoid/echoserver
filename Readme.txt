9a74995a67c502645d5c74b011950719e576886d
ZhangQianyu <zhangqianyu@oslab-Ubuntu16.04>
2019-02-21 15:22:40 +0800

    split client to two threads

--- Readme.txt
+++ Readme.txt
@@ -2,3 +2,13 @@
 
 echoserver using standard C network libraries and pthread with one thread per connection model
 
+
+
+
+synchronized editon
+
+    sender and recver on a conneciton is synchronized.
+
+    two varients buf_size and cons control the speed of send data.
+
+
--- client.c
+++ client.c
@@ -13,37 +13,80 @@
 #define MAX_CONS 1024
 #define MAX_BUF_SIZE 40960
 int requests = 1e4;
+//int send_req, recv_req;
 int cons = 10;
 int buf_size = 4096;
 int mistakes = 0;
 
 int socks[MAX_CONS];
-pthread_t tids[MAX_CONS];
+pthread_t senders[MAX_CONS], recvers[MAX_CONS];
 int con_num = 0;
 
-void* work(void* arg)
+
+
+// void* work(void* arg)
+// {
+//     int sockfd = *(int*)arg;
+//     int req;
+//     int send_len, recv_len;
+//     char buf[MAX_BUF_SIZE];
+
+//     for (int i = 0; i < buf_size; i += 4)
+//     {   *((int*)(buf + i)) = 0x12345678;
+//     }
+    
+//     while ((req = --requests) > 0)
+//     {
+//         //printf("req: %d\n", req);
+
+//         send_len = send(sockfd, buf, buf_size, 0);
+
+//         recv_len = recvfull(sockfd, buf, buf_size, 0);
+ 
+//         //printf("%d %d\n", recv_len, *(int*)buf);
+//         if  (*(int*)buf != 0x12345678)  ++mistakes;
+//     }
+    
+//     return NULL;
+// }
+
+
+void* sender(void* arg)
 {
     int sockfd = *(int*)arg;
-    int req;
-    int send_len, recv_len;
+    int req = requests;
+    int send_len;
     char buf[MAX_BUF_SIZE];
 
     for (int i = 0; i < buf_size; i += 4)
     {   *((int*)(buf + i)) = 0x12345678;
     }
     
-    while ((req = --requests) > 0)
+    while (req-- > 0)
     {
-        //printf("req: %d\n", req);
+        //printf("sender: %d\n", req);
+        send_len = sendfull(sockfd, buf, buf_size, 0);
+    }
 
-        send_len = send(sockfd, buf, buf_size, 0);
+    return NULL;
+}
 
+void* recver(void* arg)
+{
+    int sockfd = *(int*)arg;
+    int req = requests;
+    int recv_len;
+    char buf[MAX_BUF_SIZE];
+
+    while (req-- > 0)
+    {
+        //printf("recver: %d\n", req);
         recv_len = recvfull(sockfd, buf, buf_size, 0);
- 
-        //printf("%d %d\n", recv_len, *(int*)buf);
+
+
         if  (*(int*)buf != 0x12345678)  ++mistakes;
     }
-    
+
     return NULL;
 }
 
@@ -53,16 +96,19 @@ int main(int argc, char** argv)
     struct hostent* he;
     struct sockaddr_in their_addr;
 
-    if  (argc < 3)  error("usage: client <hostname> <request num> [<thread num> <buf size>]");
-    sscanf(argv[2], "%d", &requests);
-    printf("client %s %d\n", argv[1], requests);
+    if  (argc < 3)  error("usage: client <hostname> <request num> [<connection num> <buf size>]");
 
     if  ((he = gethostbyname(argv[1])) == NULL)  error("gethostbyname");
 
+    sscanf(argv[2], "%d", &requests);
+    //send_req = requests;  recv_req = requests;
+    printf("client %s %d\n", argv[1], requests);
+
+
     if  (argc >= 4)  sscanf(argv[3], "%d", &cons);
     if  (argc >= 5)  sscanf(argv[4], "%d", &buf_size);
-    
-    
+
+
     for (int i = 0; i < cons; i++)
     {
         if  ((socks[con_num] = socket(AF_INET, SOCK_STREAM, 0)) == -1)  error("socket");
@@ -74,7 +120,9 @@ int main(int argc, char** argv)
 
         if  (connect(socks[con_num], (struct sockaddr*)&their_addr, sizeof(struct sockaddr)) == -1)  error("connect");
 
-        if  (pthread_create(&tids[con_num], NULL, work, &socks[con_num]) == -1)  error("pthread_create");
+        //if  (pthread_create(&tids[con_num], NULL, work, &socks[con_num]) == -1)  error("pthread_create");
+        if  (pthread_create(&senders[con_num], NULL, sender, &socks[con_num]) == -1)  error("pthread_create sender");
+        if  (pthread_create(&recvers[con_num], NULL, recver, &socks[con_num]) == -1)  error("pthread_create recver");
 
         con_num++;
 
@@ -84,7 +132,9 @@ int main(int argc, char** argv)
 
     for (int i = 0; i < cons; i++)
     {
-        pthread_join(tids[i], NULL);
+        //pthread_join(tids[i], NULL);
+        pthread_join(senders[i], NULL);
+        pthread_join(recvers[i], NULL);
     }
 
     printf("mistakes: %d\n", mistakes);
